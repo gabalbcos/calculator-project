e53cd0e84bd10837baf01167db28bb6e556ed627
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-25 21:48:01 -0300

    Add initial js functions

---
+++ script.js
@@ -0,0 +1,68 @@
+let firstInput = "" ;
+let secondInput  = "" ;
+let operation = '';
+
+const buttonSymbol = document.querySelectorAll('.calculator__number-symbol');
+const buttonNumber = document.querySelectorAll('.calculator__number-button');
+const screenNumber = document.querySelector('.calculator__screen')
+const clearButton = document.querySelector('.ac');
+
+
+buttonNumber.forEach((button) => button.addEventListener('click', writeValue));
+buttonSymbol.forEach((button) => button.addEventListener('click', writeOperation));
+clearButton.addEventListener('click', clearAll);
+
+function writeOperation(){
+    if(operation === "" && firstInput === '0'){
+        return; 
+    } else if (operation === "" && firstInput !== '') {
+        operation = this.value;
+        return screenNumber.innerText+= this.value;
+     }else {
+       calculate();
+       return operation = this.value;
+     }
+}
+
+
+function writeValue(){
+  if(operation === ""){  
+    if(screenNumber.innerText=== '0'){
+        screenNumber.innerText="";
+        screenNumber.innerText+= this.value;
+        return firstInput+= this.value;
+    } 
+    else (screenNumber.innerText !== '0')
+        screenNumber.innerText+= this.value;
+        return firstInput+= this.value;
+    }
+
+    screenNumber.innerText+= this.value;
+    return secondInput+= this.value;
+  }
+
+
+
+
+function clearAll(){
+    screenNumber.innerText= "0";
+}
+
+
+
+function calculate(){
+    let valueHolder = "";
+    if (operation !== '' && secondInput === ''){
+        return
+    }
+    
+    switch (operation) {
+        case '+':
+        valueHolder = parseInt(firstInput) + parseInt(secondInput)
+        firstInput = valueHolder;
+        secondInput = '';
+        operation = ''
+        screenNumber.innerText= firstInput;
+        break
+    }
+}
