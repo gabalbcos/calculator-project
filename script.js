7129812479784fefafef75b35d500be5c2789512
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-26 22:15:51 -0300

    Add more style changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let firstInput = "" ;
-let secondInput  = "" ;
+let firstInput = "";
+let secondInput = "";
 let operation = '';
 
 const buttonSymbol = document.querySelectorAll('.calculator__number-symbol');
@@ -19,184 +19,184 @@ clearButton.addEventListener('click', clearAll);
 percentButton.addEventListener('click', percentual);
 eraseButton.addEventListener('click', erase);
 
-function writeOperation(){
-    if(operation === "" && firstInput === '0'  ){
-        return; 
-    }else if (operation === "" && firstInput !== ''){
+function writeOperation() {
+    if (operation === "" && firstInput === '0') {
+        return;
+    } else if (operation === "" && firstInput !== '') {
         operation = this.value;
-        return screenNumber.innerText+= this.value;
-     }else {
-       calculate();
-       return operation = this.value;
-     }
+        return screenNumber.innerText += this.value;
+    } else {
+        calculate();
+        return operation = this.value;
+    }
 }
 
 
 
 
-function writeValue(){
-  if(operation === "" ){  
-    if(screenNumber.innerText === 0){
+function writeValue() {
+    if (operation === "") {
+        if (screenNumber.innerText === 0) {
 
-        firstInput+= this.value;
+            firstInput += this.value;
+            return screenNumber.innerText = firstInput;
+        } else(screenNumber.innerText !== 0)
+        firstInput += this.value;
         return screenNumber.innerText = firstInput;
-    } 
-    else (screenNumber.innerText !== 0)
-         firstInput+= this.value;
-        return screenNumber.innerText = firstInput;
-         
+
     }
 
-    screenNumber.innerText+= this.value;
-    return secondInput+= this.value;
+    screenNumber.innerText += this.value;
+    return secondInput += this.value;
 
 }
 
 
-function clearAll(){
-    screenNumber.innerText= "0";
-    resultNumber.innerText= "0";
-    firstInput = "" ;
-    secondInput  = "" ;
+function clearAll() {
+    screenNumber.innerText = "0";
+    resultNumber.innerText = "0";
+    firstInput = "";
+    secondInput = "";
     operation = '';
 }
 
 
 
-function calculate(){
+function calculate() {
     let valueHolder = "";
-    if (operation !== '' && secondInput === ''){
+    if (operation !== '' && secondInput === '') {
         return
-    }else if(Number.isInteger(+firstInput)){
-    
-    switch (operation) {
-        case '+':
-        writeResult();
-        valueHolder = parseInt(firstInput) + parseInt(secondInput)
-        firstInput = valueHolder;
-        secondInput = '';
-        operation = ''
-        screenNumber.innerText= firstInput;
-        break
+    } else if (Number.isInteger(+firstInput)) {
 
-        case '-':
-            writeResult();
-            valueHolder = parseInt(firstInput) - parseInt(secondInput)
-            firstInput = valueHolder;
-            secondInput = '';
-            operation = ''
-            screenNumber.innerText= firstInput;
-            break
-            
-        case '/':
-            writeResult();
-            valueHolder = parseInt(firstInput) / parseInt(secondInput)
-            firstInput = valueHolder;
-            secondInput = '';
-
-            operation = ''
-            screenNumber.innerText= firstInput;
-            break
-
-           case '*':
-            writeResult();
-            valueHolder = parseInt(firstInput) * parseInt(secondInput);
-            firstInput = valueHolder;
-            secondInput = '';
-            operation = ''
-            screenNumber.innerText= firstInput;
-            break   
-                
-            case 'percent':
+        switch (operation) {
+            case '+':
                 writeResult();
-                valueHolder =  parseFloat(firstInput).toFixed(2)/100;
+                valueHolder = parseInt(firstInput) + parseInt(secondInput)
                 firstInput = valueHolder;
                 secondInput = '';
-                screenNumber.innerText= firstInput;
-                break    
+                operation = ''
+                screenNumber.innerText = firstInput;
+                break
+
+            case '-':
+                writeResult();
+                valueHolder = parseInt(firstInput) - parseInt(secondInput)
+                firstInput = valueHolder;
+                secondInput = '';
+                operation = ''
+                screenNumber.innerText = firstInput;
+                break
+
+            case '/':
+                writeResult();
+                valueHolder = parseInt(firstInput) / parseInt(secondInput)
+                firstInput = valueHolder;
+                secondInput = '';
+
+                operation = ''
+                screenNumber.innerText = firstInput;
+                break
+
+            case '*':
+                writeResult();
+                valueHolder = parseInt(firstInput) * parseInt(secondInput);
+                firstInput = valueHolder;
+                secondInput = '';
+                operation = ''
+                screenNumber.innerText = firstInput;
+                break
+
+            case 'percent':
+                writeResult();
+                valueHolder = parseFloat(firstInput).toFixed(2) / 100;
+                firstInput = valueHolder;
+                secondInput = '';
+                screenNumber.innerText = firstInput;
+                break
             case 'e':
                 valueHolder = parseInt(firstInput) ** parseInt(secondInput);
                 firstInput = valueHolder;
                 secondInput = '';
                 operation = ''
-                screenNumber.innerText= firstInput;                         
-        }} else     switch (operation) {
-            case '+':
+                screenNumber.innerText = firstInput;
+        }
+    } else switch (operation) {
+        case '+':
             writeResult();
             valueHolder = parseFloat(firstInput) + parseFloat(secondInput)
             firstInput = valueHolder;
             secondInput = '';
             operation = ''
-            screenNumber.innerText= firstInput.toFixed(2);
+            screenNumber.innerText = firstInput.toFixed(2);
             break
-    
-            case '-':
-                writeResult();
-                valueHolder = parseFloat(firstInput) - parseFloat(secondInput)
-                firstInput = valueHolder;
-                secondInput = '';
-                operation = ''
-                screenNumber.innerText= firstInput.toFixed(2);
-                break
-                
-            case '/':
-                writeResult();
-                valueHolder = parseFloat(firstInput) / parseFloat(secondInput)
-                firstInput = valueHolder;
-                secondInput = '';
-    
-                operation = ''
-                screenNumber.innerText= firstInput.toFixed(2);
-                break
-    
-               case '*':
-                writeResult();
-                valueHolder = parseFloat(firstInput) * parseFloat(secondInput);
-                firstInput = valueHolder;
-                secondInput = '';
-                operation = ''
-                screenNumber.innerText= firstInput.toFixed(2);
-                break   
-                    
-                case 'percent':
-                    writeResult();
-                    valueHolder =  parseFloat(firstInput).toFixed(2)/100;
-                    firstInput = valueHolder;
-                    secondInput = '';
-                    screenNumber.innerText= firstInput.toFixed(2);
-                    break    
-                case 'e':
-                    valueHolder = parseFloat(firstInput) ** parseFloat(secondInput);
-                    firstInput = valueHolder;
-                    secondInput = '';
-                    operation = ''
-                    screenNumber.innerText= firstInput.toFixed(2);                         
-            }
+
+        case '-':
+            writeResult();
+            valueHolder = parseFloat(firstInput) - parseFloat(secondInput)
+            firstInput = valueHolder;
+            secondInput = '';
+            operation = ''
+            screenNumber.innerText = firstInput.toFixed(2);
+            break
+
+        case '/':
+            writeResult();
+            valueHolder = parseFloat(firstInput) / parseFloat(secondInput)
+            firstInput = valueHolder;
+            secondInput = '';
+
+            operation = ''
+            screenNumber.innerText = firstInput.toFixed(2);
+            break
+
+        case '*':
+            writeResult();
+            valueHolder = parseFloat(firstInput) * parseFloat(secondInput);
+            firstInput = valueHolder;
+            secondInput = '';
+            operation = ''
+            screenNumber.innerText = firstInput.toFixed(2);
+            break
+
+        case 'percent':
+            writeResult();
+            valueHolder = parseFloat(firstInput).toFixed(2) / 100;
+            firstInput = valueHolder;
+            secondInput = '';
+            screenNumber.innerText = firstInput.toFixed(2);
+            break
+        case 'e':
+            valueHolder = parseFloat(firstInput) ** parseFloat(secondInput);
+            firstInput = valueHolder;
+            secondInput = '';
+            operation = ''
+            screenNumber.innerText = firstInput.toFixed(2);
+    }
 
 }
 
-function percentual(){
-   
-    firstInput =  parseFloat(firstInput).toFixed(2)/100;
+function percentual() {
+
+    firstInput = parseFloat(firstInput).toFixed(2) / 100;
     operator = '';
-    screenNumber.innerText= firstInput; 
+    screenNumber.innerText = firstInput;
 }
 
-function writeResult(){
-    resultNumber.innerText= screenNumber.innerText;
+function writeResult() {
+    resultNumber.innerText = screenNumber.innerText;
 }
 
 
-function erase(){
-    if (operation === ""){
+function erase() {
+    if (operation === "") {
         firstInput = firstInput.slice(0, -1);
         screenNumber.innerText = firstInput;
-    } else if (operation !== "" && secondInput === ''){
+    } else if (operation !== "" && secondInput === '') {
         operation = operation.slice(0, -1);
         screenNumber.innerText = screenNumber.innerText.slice(0, -1);
-    }else {
+    } else {
         secondInput = secondInput.slice(0, -1);
         screenNumber.innerText = screenNumber.innerText.slice(0, -1);
-           
+
     }
 }
